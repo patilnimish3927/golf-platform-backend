439f01d0bf0bd9bfef0b46f809799aab1d9ee961
patilnimish3927 <patilnimish3927@gmail.com>
2026-03-31 00:11:57 +0530

    fixed bug

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const { verifyUser } = require('../middlewares/auth')
-const { addScore } = require('../controllers/userController')
-
+const { addScore, getScores } = require('../controllers/userController')
 router.post('/score', verifyUser, addScore)
 router.get('/scores', verifyUser, getScores)
 module.exports = router
