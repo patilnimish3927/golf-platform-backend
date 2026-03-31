06f4fd34dff484ad1f646e9b8f3fed54cabaf689
patilnimish3927 <patilnimish3927@gmail.com>
2026-03-31 11:46:56 +0530

    test

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -19,7 +19,8 @@ exports.runDraw = async (req, res) => {
     return nums
   }
 
-  const numbers = generateNumbers()
+  const numbers = [1,2,3,4,5]
+  // const numbers = generateNumbers()
 
   const { data: drawData } = await supabase
     .from('draws')
